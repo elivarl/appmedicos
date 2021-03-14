d3f766bf04e30bc195ac5d7abf4377c88ba85fd6
elivar <elargor@gmail.com>
2021-03-14 11:56:32 -0500

    arquetipo inicial

---
+++ src/main/java/com/ecodeup/appmedicos/AppmedicosApplication.java
@@ -0,0 +1,14 @@
+package com.ecodeup.appmedicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
+public class AppmedicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmedicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecodeup/appmedicos/AppmedicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecodeup.appmedicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppmedicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
