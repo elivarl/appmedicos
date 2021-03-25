04cb1a09c254d45bbecfb26dd450bf62bd02d690
elivar <elargor@gmail.com>
2021-03-24 23:25:17 -0500

    añadido template admin 2 de boostrap, vista index y create con los métodos en el controller

--- src/main/java/com/ecodeup/appmedicos/controller/PacienteController.java
+++ src/main/java/com/ecodeup/appmedicos/controller/PacienteController.java
@@ -1,10 +1,29 @@
 package com.ecodeup.appmedicos.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ecodeup.appmedicos.entity.Paciente;
+
 @Controller
-@RequestMapping("paciente")
+@RequestMapping("/paciente")
 public class PacienteController {
+	
+	@GetMapping("/index")
+	public String index() {
+		return "paciente/index";
+	}
+	
+	@GetMapping("/nuevo")
+	public String nuevo() {
+		return "paciente/create";
+	}
+	
+	@PostMapping("/save")
+	public String save (Paciente paciente) {
+		return "redirect:/index";
+	}
 	
 }
