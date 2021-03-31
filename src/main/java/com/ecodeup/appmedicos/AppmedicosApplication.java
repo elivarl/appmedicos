8c6b229fd3495d91f998da01d20ef104563bd207
Elivar <elargor@gmail.com>
2021-03-31 09:36:26 -0500

    adaptación a bd julian

--- src/main/java/com/ecodeup/appmedicos/AppmedicosApplication.java
+++ src/main/java/com/ecodeup/appmedicos/AppmedicosApplication.java
@@ -3,7 +3,7 @@ package com.ecodeup.appmedicos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // (exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication //(exclude = DataSourceAutoConfiguration.class)
 public class AppmedicosApplication {
 
 	public static void main(String[] args) {
