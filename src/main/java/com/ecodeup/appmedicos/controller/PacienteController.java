cb285ecf54977b55524c4567bbfaefef8ae74d21
Elivar <elargor@gmail.com>
2021-03-31 10:26:51 -0500

    funcionalidad para guardar un paciente y mostrar los pacientes en la vista index

--- src/main/java/com/ecodeup/appmedicos/controller/PacienteController.java
+++ src/main/java/com/ecodeup/appmedicos/controller/PacienteController.java
@@ -2,21 +2,28 @@ package com.ecodeup.appmedicos.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecodeup.appmedicos.entity.Paciente;
+import com.ecodeup.appmedicos.service.IPacienteService;
 
 @Controller
 @RequestMapping("/paciente")
 public class PacienteController {
 	private Logger logger= LoggerFactory.getLogger(PacienteController.class);
 	
+	@Autowired
+	private IPacienteService pacienteService;
+	
 	
 	@GetMapping("/index")
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("pacientes", pacienteService.findAll());
 		return "paciente/index";
 	}
 	
@@ -28,6 +35,7 @@ public class PacienteController {
 	@PostMapping("/save")
 	public String save (Paciente paciente) {
 		logger.info("Información del paciente {}", paciente);
+		pacienteService.save(paciente);
 		return "redirect:/paciente/index";
 	}
 	
